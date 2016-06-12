dde46d7205d59f1b8f0fcf4ab38c381a71d598e1
DAB <dab@dab.la>
2016-06-12 14:18:12 -0500

    added docblocks

--- Model/Address.php
+++ Model/Address.php
@@ -4,6 +4,24 @@ namespace Modules\Checkout\Model;
 
 use Lightning\Model\Object;
 
+/**
+ * Class Address
+ *   Can be a shipping or billing address.
+ *
+ * @package Modules\Checkout\Model
+ *
+ * @parameter integer $id
+ *   The primary key alias.
+ * @parameter integer $address_id
+ *   The primary key.
+ * @parameter string $name
+ * @parameter string $street
+ * @parameter string $street2
+ * @parameter string $city
+ * @parameter string $state
+ * @parameter string $zip
+ * @parameter string $country
+ */
 class Address extends Object {
     const TABLE = 'checkout_address';
     const PRIMARY_KEY = 'address_id';
