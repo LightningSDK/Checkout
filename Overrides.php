429cc2a0546aebb3baebe433f9163c07756209a7
DAB <dab@dab.la>
2016-09-03 22:14:57 -0500

    added overrides file

---
+++ Overrides.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * @file
+ * These are here to map the overridable classes for your IDE.
+ */
+
+namespace Modules\Checkout\Model;
+
+class Order extends \Modules\Checkout\Model\OrderOverridable {}
