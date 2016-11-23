86939bc514e5890ceb0c815a87ff1656264c123f
DAB <dab@dab.la>
2016-11-23 10:06:22 -0600

    Changed category page to block layout

--- Templates/category.tpl.php
+++ Templates/category.tpl.php
@@ -11,11 +11,11 @@
         <a href=""></a>
     </div>
 </div>
-<div class="row">
+<ul class="small-block-grid-2 medium-block-grid-3 large-block-grid-4">
     <?php foreach ($products as $product): ?>
-    <div class="column small-12 medium-4 large-3 left">
+    <li class="column small-12 medium-4 large-3 left">
         <a href="/store/<?= $product->url; ?>"><img src="<?= $product->getImage(); ?>">
         <br><strong><?=$product->title;?></strong></a>
-    </div>
+    </li>
     <?php endforeach; ?>
-</div>
+</ul>
