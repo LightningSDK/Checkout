b5975c9d94982851ecdcb6c3a93050c070996b66
DAB <dab@dab.la>
2016-11-24 20:03:51 -0600

    added description to default product layout

--- Templates/default_options_layout.tpl.php
+++ Templates/default_options_layout.tpl.php
@@ -1,6 +1,7 @@
 <div class="medium-6 column options-image">
 </div>
 <div class="medium-6 column options-fields">
+    <p><?= $product->description; ?></p>
     <div class="children"></div>
     <div class="price text-right"></div>
 </div>
