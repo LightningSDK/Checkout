f976473dc376d274294332b2a7e7b065c4fa4e0a
DAB <dab@dab.la>
2018-01-21 12:36:06 -0600

    Added missing template and shipped image

---
+++ Templates/confirm_mark_shipped.tpl.php
@@ -0,0 +1,15 @@
+<div class="row">
+    <div class="column">
+        <form method="post">
+            <?= \Lightning\Tools\Form::renderTokenInput(); ?>
+            <h3>
+                Are you sure you want to mark this order as fulfilled?
+            </h3>
+            <input type="hidden" name="id" value="<?= $order_id; ?>">
+            <div>
+                <a href="/admin/orders" class="button medium">No</a>
+                <input type="submit" name="submit" value="Mark as Fulfilled" class="button medium red">
+            </div>
+        </form>
+    </div>
+</div>
