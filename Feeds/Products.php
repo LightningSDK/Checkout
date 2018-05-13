acc7b97d8622867c81951592e755e9e3a1de4299
DAB <dab@dab.la>
2018-05-12 22:14:38 -0500

    Added product feed

---
+++ Feeds/Products.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace Modules\Checkout\Feeds;
+
+use Lightning\Model\URL;
+use Lightning\View\Feed;
+use Modules\Checkout\Model\Product;
+
+class Products extends Feed {
+
+    const NAME = 'products';
+
+    public function hasAccess() {
+        return true;
+    }
+
+    public function load() {
+        $this->cursor = Product::loadAll(['active' => 1]);
+    }
+
+    public function getCSVHeaders() {
+        return [
+            'Title',
+            'SKU',
+            'Category',
+            'Image URL',
+        ];
+    }
+
+    /**
+     * @param Product $product
+     *
+     * @return array
+     */
+    public function next($product) {
+        return [
+            'title' => $product->title,
+            'SKU' => $product->sku,
+            'category' => $product->getCategory()->name,
+            'image_url' => URL::getAbsolute($product->getImage()),
+        ];
+    }
+}
